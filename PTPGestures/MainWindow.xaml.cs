c778674edbb7921551c13f29103d9d18655a5b02
aimlessc <chumkky@gmail.com>
2015-01-11 19:17:33 -0700

    First prototype; Captures touchpad input

---
+++ PTPGestures/MainWindow.xaml.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Interop;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+
+namespace PTPGestures
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IntPtr hwnd; 
+        public MainWindow()
+        {
+            InitializeComponent();
+            //RawInput input = new RawInput(this);
+        }
+
+        public struct TouchData
+        {
+            public UInt16 ContactAmount;
+            public UInt16 Time; // ???
+            public UInt16 Y;
+            public UInt16 X;
+            public UInt16 Status;
+        }
+
+        public enum StatusType : ushort
+        {
+            CONTACT1_DOWN = 0x0303,
+            CONTACT1_UP = 0x0103,
+            CONTACT2_DOWN = 0x0703,
+            CONTACT2_UP = 0x0503,
+            CONTACT3_DOWN = 0x0b03,
+            CONTACT3_UP = 0x0903
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            RawInputAPI.RawInputDevice[] rid = new RawInputAPI.RawInputDevice[1];
+            rid[0].UsagePage = RawInputAPI.HIDUsagePage.Digitizer;
+            rid[0].Usage = RawInputAPI.HIDUsage.Gamepad;
+            rid[0].Flags = RawInputAPI.RawInputDeviceFlags.InputSink;
+            rid[0].WindowHandle = (new WindowInteropHelper(this)).Handle;
+            //txt.Text += "Handle: " + rid[0].WindowHandle.ToInt32() + "\n";
+            if (RawInputAPI.RegisterRawInputDevices(rid, (uint)rid.Length, (uint)Marshal.SizeOf(rid[0])) == false)
+            {
+                //txt.Text += "ERROR: Unable to register digitizer.\n";
+                //txt.Text += (new Win32Exception(Marshal.GetLastWin32Error())).Message + "\n";
+            }
+            else
+            {
+                //txt.Text += "Registered digitizer!\n";
+            }
+        }
+
+        private void OnSourceInitialized(object sender, EventArgs e)
+        {
+            // Hook WndProc
+            IntPtr hwnd = new WindowInteropHelper(this).Handle;
+            HwndSource.FromHwnd(hwnd).AddHook(new HwndSourceHook(WndProc));
+        }
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        //protected override void WndProc(ref Message m)
+        {
+            // Handle messages...
+            switch (msg)
+            {
+                case (int)(RawInputAPI.WindowsMessages.Input):
+                    int size = Marshal.SizeOf(typeof(RawInputAPI.RawInput));
+                    RawInputAPI.RawInput inputData = new RawInputAPI.RawInput();
+                    int insize = RawInputAPI.GetRawInputData(lParam, RawInputAPI.RawInputCommand.Input, out inputData, ref size, Marshal.SizeOf(typeof(RawInputAPI.RawInputHeader)));
+                    if (insize == -1)
+                    {
+                        //txt.Text += (new Win32Exception(Marshal.GetLastWin32Error())).Message + "\n";
+                    }
+                    else
+                    {
+                        string s = "";
+                        //TouchData theData;
+                        //theData = (TouchData)Marshal.PtrToStructure(inputData.HID.Data, typeof(TouchData));
+                        //txt.Text += "Size:" + insize + ":" + size + "; Data:" + inputData.HID.Data.ToString("X") + "\n";
+                        if (false || inputData.Header.Type == RawInputAPI.RawInputType.HID)
+                        {
+                            byte[] wholedata = new byte[inputData.HID.Size * inputData.HID.Count];
+                            //Marshal.Copy(inputData.HID.Data, wholedata, 0, inputData.HID.Size * inputData.HID.Count);
+                            //txt.Text += "Size:" + insize + ":" + size + "; Data: ";
+                            //foreach (Byte b in wholedata) txt.Text += b.ToString("X");
+                            //txt.Text += "\n";
+                        }
+                        s = "Data:" + inputData.HID.Data3.ToString("X4") + inputData.HID.Data2.ToString("X8") + inputData.HID.Data.ToString("X8");
+                        //s = "Data:" + inputData.HID.Data.ToString("X10") + inputData.Mouse.LastX.ToString("X8") + inputData.Mouse.LastY.ToString("X4") + ", " + inputData.Header.Size;
+                        TouchData data = new TouchData();
+                        data.ContactAmount = (UInt16)(inputData.HID.Data3.ToInt32() & 0xffff);
+                        data.Time = (UInt16)(inputData.HID.Data2.ToInt32() >> 16);
+                        data.Y = (UInt16)(inputData.HID.Data2.ToInt32() & 0xffff);
+                        data.X = (UInt16)(inputData.HID.Data.ToInt32() >> 16);
+                        data.Status = (UInt16)(inputData.HID.Data.ToInt32() & 0xffff);
+                        s = "Contacts: " + data.ContactAmount.ToString("X") + "; Time: " + data.Time.ToString("X4") + "; Status: " + data.Status.ToString("X4") + "; X: " + data.X.ToString("X4") + "; Y: " + data.Y.ToString("X4");
+                        Console.WriteLine(s);
+
+                        switch ((StatusType)data.Status)
+                        {
+                            case StatusType.CONTACT1_DOWN:
+                                if(!Contact1.Points.Any(p => p.X == data.X && p.Y == data.Y))
+                                    Contact1.Points.Add(new Point(data.X, data.Y));
+                                break;
+                            case StatusType.CONTACT1_UP:
+                                Contact1.Points.Clear();
+                                break;
+                            case StatusType.CONTACT2_DOWN:
+                                if(!Contact2.Points.Any(p => p.X == data.X && p.Y == data.Y))
+                                    Contact2.Points.Add(new Point(data.X, data.Y));
+                                break;
+                            case StatusType.CONTACT2_UP:
+                                Contact2.Points.Clear();
+                                break;
+                            case StatusType.CONTACT3_DOWN:
+                                if (!Contact3.Points.Any(p => p.X == data.X && p.Y == data.Y))
+                                    Contact3.Points.Add(new Point(data.X, data.Y));
+                                break;
+                            case StatusType.CONTACT3_UP:
+                                Contact3.Points.Clear();
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+            //scrollarea.ScrollToBottom();
+            return IntPtr.Zero;
+            //return base.WndProc(hwnd, msg, wParam, lParam, ref handled);
+        }
+
+        private void OnClosing(object sender, CancelEventArgs e)
+        {
+            // Remove Hook
+            HwndSource.FromHwnd((new WindowInteropHelper(this)).Handle).RemoveHook(new HwndSourceHook(WndProc));
+        }
+    }
+}
---
+++ PTPGestures/RawInput.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace PTPGestures
+{
+    class RawInputAPI
+    {
+        public const int RID_INPUT = 0x10000003;
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RawInputHeader
+        {
+            public RawInputType Type;
+            public int Size;
+            public IntPtr Device;
+            public IntPtr wParam;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RawInputHID
+        {
+            public int Size;
+            public int Count;
+            public IntPtr Data;  // For some reason data is being put in rather than addresses? Why?
+            public IntPtr Data2; // Probably the bytes are listed sequentially in memory rather than elsewhere.
+            public IntPtr Data3; // This hack will work for now.
+        }
+ 
+        [StructLayout(LayoutKind.Explicit)]
+        public struct RawInput
+        {
+            [FieldOffset(0)]
+            public RawInputHeader Header;
+            [FieldOffset(16)]        
+            public RawMouse Mouse;
+            [FieldOffset(16)]
+            public RawKeyboard Keyboard;
+            [FieldOffset(16)]
+            public RawInputHID HID;
+        }
+        /*
+        {
+            public RawInputHeader Header;
+            public Union Data;
+            [StructLayout(LayoutKind.Explicit)]
+            public struct Union
+            {
+                [FieldOffset(0)]
+                public RawMouse Mouse;
+                [FieldOffset(0)]
+                public RawKeyboard Keyboard;
+                [FieldOffset(0)]
+                public RawInputHID HID;
+            }
+        }
+         */
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RawMouse
+        {
+            public RawMouseFlags Flags;
+            [StructLayout(LayoutKind.Explicit)]
+            public struct Data
+            {
+                [FieldOffset(0)]
+                public uint Buttons;
+                [FieldOffset(2)]
+                public ushort ButtonData;
+                [FieldOffset(0)]
+                public RawMouseButtons ButtonFlags;
+            };
+            public Data m_Data;
+            public uint RawButtons;
+            public int LastX;
+            public int LastY;
+            public uint ExtraInformation;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RawInputDevice
+        {
+            public HIDUsagePage UsagePage;
+            public HIDUsage Usage;
+            public RawInputDeviceFlags Flags;
+            public IntPtr WindowHandle;
+        }
+
+        public enum HIDUsage : ushort
+        {
+            Pointer = 0x01,
+            Mouse = 0x02,
+            Joystick = 0x04,
+            Gamepad = 0x05,
+        }
+
+        public enum HIDUsagePage : ushort
+        {
+            Digitizer = 0x0D
+        }
+
+        [Flags]
+        public enum RawKeyboardFlags : ushort
+        {
+            KeyMake = 0,
+            KeyBreak = 1,
+            KeyE0 = 2,
+            KeyE1 = 4,
+            TerminalServerSetLED = 8,
+            TerminalServerShadow = 0x10,
+            TerminalServerVKPACKET = 0x20
+        }
+
+        [Flags()]
+        public enum RawInputDeviceFlags : int
+        {
+            None = 0,
+            Remove = 0x00000001,
+            Exclude = 0x00000010,
+            PageOnly = 0x00000020,
+            NoLegacy = 0x00000030,
+            InputSink = 0x00000100,
+            CaptureMouse = 0x00000200,
+            NoHotKeys = 0x00000200,
+            AppKeys = 0x00000400
+        }
+
+        [Flags()]
+        public enum RawMouseFlags
+            : ushort
+        {
+            MoveRelative = 0,
+            MoveAbsolute = 1,
+            VirtualDesktop = 2,
+            AttributesChanged = 4
+        }
+
+        public enum RawInputType
+        {
+            Mouse = 0,
+            Keyboard = 1,
+            HID = 2,
+            Other = 3
+        }
+
+        public enum RawInputCommand
+        {
+            Input = 0x10000003,
+            Header = 0x10000005
+        }
+
+        [Flags()]
+        public enum RawMouseButtons
+            : ushort
+        {
+            None = 0,
+            LeftDown = 0x0001,
+            LeftUp = 0x0002,
+            RightDown = 0x0004,
+            RightUp = 0x0008,
+            MiddleDown = 0x0010,
+            MiddleUp = 0x0020,
+            Button4Down = 0x0040,
+            Button4Up = 0x0080,
+            Button5Down = 0x0100,
+            Button5Up = 0x0200,
+            MouseWheel = 0x0400
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RawKeyboard
+        {
+            public short MakeCode;
+            public RawKeyboardFlags Flags;
+            public short Reserved;
+            public VirtualKeys VirtualKey;
+            public WindowsMessages Message;
+            public int ExtraInformation;
+        }
+
+        public enum VirtualKeys // Don't think I really need this
+            : ushort
+        {
+            LeftButton = 0x01,
+        }
+
+        public enum WindowsMessages : uint
+        {
+            Input = 0x00FF,
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct DeviceInfo {
+            [FieldOffset(0)]
+            public int Size;
+            [FieldOffset(1)]
+            public int Type;
+            //[FieldOffset(2)]
+            //public int
+        };
+
+        [DllImport("user32.dll",SetLastError=true)]
+        public static extern int GetRawInputData(IntPtr hRawInput, RawInputCommand uiCommand, out RawInput pData, ref int pcbSize, int cbSizeHeader);
+        [DllImport("user32.dll")]
+        public static extern uint GetRawInputDeviceInfo(IntPtr hDevice, uint uiCommand, IntPtr pData, ref uint pcbSize);
+        public enum RawInputDeviceType : uint
+        {
+            MOUSE = 0,
+            KEYBOARD = 1,
+            HID = 2
+        }
+        [DllImport("user32.dll",SetLastError=true)]
+        public static extern bool RegisterRawInputDevices([MarshalAs(UnmanagedType.LPArray, SizeParamIndex=0)] RawInputDevice[] pRawInputDevices, uint uiNumDevices, uint cbSize);
+
+    }
+};
